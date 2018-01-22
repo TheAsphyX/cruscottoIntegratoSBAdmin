5aa814de18746e0b0225707263b9810ccda0acbf
luigi frasca <luigi.frasca@gmail.com>
2018-01-22 07:25:56 +0100

    aggiunto componente di prova.

--- src/app/layout/blank-page/blank-page-routing.module.ts
+++ src/app/layout/blank-page/blank-page-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BlankPageComponent } from './blank-page.component';
+import { BlankPageComponentDett } from './blank-page-dett.component';
 
 const routes: Routes = [
     {
         path: '',
         component: BlankPageComponent
+    },
+    {
+        path: 'blank-page-dett-ro',
+        component: BlankPageComponentDett
     }
+
 ];
 
 @NgModule({
