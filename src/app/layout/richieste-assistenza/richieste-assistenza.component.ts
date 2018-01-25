6f0141797c28758bb192edf05b770c27c2237c54
luigi frasca <luigi.frasca@gmail.com>
2018-01-25 19:00:40 +0100

    Completata integrazione richieste di assistenza.

--- src/app/layout/richieste-assistenza/richieste-assistenza.component.ts
+++ src/app/layout/richieste-assistenza/richieste-assistenza.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 
+import { SintesiRichiesta } from "../../shared/modules/rich-ass/sintesi-richiesta/sintesi-richiesta.model";
+import { SintesiRichiesteService } from "../../shared/modules/rich-ass/sintesi-richieste-service/sintesi-richieste.service";
+
+
 @Component({
     selector: 'richieste-assistenza',
     templateUrl: './richieste-assistenza.component.html',
@@ -8,7 +12,19 @@ import { routerTransition } from '../../router.animations';
     animations: [routerTransition()]
 })
 export class RichiesteAssistenzaComponent implements OnInit {
-    constructor() {}
+    private richieste: SintesiRichiesta[] = [];
 
-    ngOnInit() {}
+    constructor(private sintesiRichiesteService: SintesiRichiesteService) {}
+
+    ngOnInit() {
+        this.sintesiRichiesteService.getSintesiRichieste()
+      .subscribe(richieste => {
+        console.log("Richieste service: ", richieste);
+        this.richieste = richieste;
+      });
+    }
+
+    showDettagliRicevuto(richiesta: SintesiRichiesta): void {
+        console.log("Sono app.component. Vogliono vedere i dettagli di", richiesta.id);
+      }
 }
