a4427aa4c695054b24acb2ffa8946491a556c63d
Luigi Frasca <luigi,frasca@vigilfuoco.it>
2018-01-31 12:37:49 +0100

    Componente mezzi in servizio, iniziata integrazione.
    Creato skeleton componente.

--- src/app/shared/modules/index.ts
+++ src/app/shared/modules/index.ts
@@ -3,3 +3,4 @@ export * from './stat/stat.module';
 export * from './info-aggregate/info-aggregate.module';
 export * from './rich-ass/rich-ass.module';
 export * from './eventi-rich-ass/eventi-rich-ass.module';
+export * from './mezzi-in-servizio/mezzi-in-servizio.module';
