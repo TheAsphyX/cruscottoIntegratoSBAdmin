d8e33f6e0264a158094fce21b89eccc8e5718022
luigi.frasca@vigilfuoco.it <luigi.frasca@laz.dipvvf.it>
2018-02-09 09:04:34 +0100

    Composizione partenza: iniziata integrazione come componente shared.

--- src/app/shared/modules/index.ts
+++ src/app/shared/modules/index.ts
@@ -4,3 +4,4 @@ export * from './info-aggregate/info-aggregate.module';
 export * from './rich-ass/rich-ass.module';
 export * from './eventi-rich-ass/eventi-rich-ass.module';
 export * from './mezzi-in-servizio/mezzi-in-servizio.module';
+export * from './composizione-partenza-shared/composizione-partenza-shared.module';
